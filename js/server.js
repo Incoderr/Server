7b01c61bd8b91ce6d804488ef213f01a4fbca604
Incoderr <laenartem75@gmail.com>
2025-02-27 14:13:45 +0500

    fav

--- js/server.js
+++ js/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: ['http://localhost:5173', 'https://animeinc.vercel.app'],
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());

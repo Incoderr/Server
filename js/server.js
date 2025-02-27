97be1d4355be71c06a171dcc42b4d0020e19edee
Incoderr <laenartem75@gmail.com>
2025-02-27 14:31:23 +0500

    del

--- js/server.js
+++ js/server.js
@@ -12,7 +12,10 @@ app.use(cors({
   origin: ['http://localhost:5173', 'https://animeinc.vercel.app'],
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+app.options('*', cors());
+
 app.use(express.json());
 
 // Подключение к MongoDB

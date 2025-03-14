d904942cf9f4e3ce7c22e3cd351e4aad43523cf4
Incoderr <laenartem75@gmail.com>
2025-03-14 18:49:42 +0500

    cors

--- js/server.js
+++ js/server.js
@@ -13,7 +13,7 @@ const { gql } = require('graphql-tag');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://animeinc.vercel.app'],
+  origin: ['http://localhost:5173', 'https://animeinc.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
